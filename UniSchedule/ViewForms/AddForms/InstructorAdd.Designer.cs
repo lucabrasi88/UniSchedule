56068ee6ea25b90baf42c14f21cbd21e08ff3eb2
Lukasz <l.czaczyk88@gmail.com>
2015-11-29 21:26:48 +0100

    Instructors adding

--- UniSchedule/ViewForms/AddForms/InstructorAdd.Designer.cs
+++ UniSchedule/ViewForms/AddForms/InstructorAdd.Designer.cs
@@ -28,20 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtPhoneOne = new System.Windows.Forms.TextBox();
+            this.lblPhoneOne = new System.Windows.Forms.Label();
+            this.lblDegree = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblRoomNo = new System.Windows.Forms.Label();
+            this.txtPhoneTwo = new System.Windows.Forms.TextBox();
+            this.lblPhoneTwo = new System.Windows.Forms.Label();
+            this.cbDegree = new System.Windows.Forms.ComboBox();
+            this.txtRoomNo = new System.Windows.Forms.TextBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnSaveInstructor = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(55, 45);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(26, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Imię";
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Location = new System.Drawing.Point(251, 45);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(53, 13);
+            this.lblSurname.TabIndex = 1;
+            this.lblSurname.Text = "Nazwisko";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(58, 62);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(164, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(254, 62);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(164, 20);
+            this.txtSurname.TabIndex = 3;
+            // 
+            // txtPhoneOne
+            // 
+            this.txtPhoneOne.Location = new System.Drawing.Point(254, 175);
+            this.txtPhoneOne.Name = "txtPhoneOne";
+            this.txtPhoneOne.Size = new System.Drawing.Size(164, 20);
+            this.txtPhoneOne.TabIndex = 7;
+            // 
+            // lblPhoneOne
+            // 
+            this.lblPhoneOne.AutoSize = true;
+            this.lblPhoneOne.Location = new System.Drawing.Point(251, 158);
+            this.lblPhoneOne.Name = "lblPhoneOne";
+            this.lblPhoneOne.Size = new System.Drawing.Size(79, 13);
+            this.lblPhoneOne.TabIndex = 5;
+            this.lblPhoneOne.Text = "Numer telefonu";
+            // 
+            // lblDegree
+            // 
+            this.lblDegree.AutoSize = true;
+            this.lblDegree.Location = new System.Drawing.Point(55, 103);
+            this.lblDegree.Name = "lblDegree";
+            this.lblDegree.Size = new System.Drawing.Size(89, 13);
+            this.lblDegree.TabIndex = 4;
+            this.lblDegree.Text = "Stopień naukowy";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(58, 175);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(164, 20);
+            this.txtEmail.TabIndex = 9;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(55, 158);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(64, 13);
+            this.lblEmail.TabIndex = 8;
+            this.lblEmail.Text = "Adres e-mail";
+            // 
+            // lblRoomNo
+            // 
+            this.lblRoomNo.AutoSize = true;
+            this.lblRoomNo.Location = new System.Drawing.Point(251, 104);
+            this.lblRoomNo.Name = "lblRoomNo";
+            this.lblRoomNo.Size = new System.Drawing.Size(73, 13);
+            this.lblRoomNo.TabIndex = 11;
+            this.lblRoomNo.Text = "Numer pokoju";
+            // 
+            // txtPhoneTwo
+            // 
+            this.txtPhoneTwo.Location = new System.Drawing.Point(58, 226);
+            this.txtPhoneTwo.Name = "txtPhoneTwo";
+            this.txtPhoneTwo.Size = new System.Drawing.Size(164, 20);
+            this.txtPhoneTwo.TabIndex = 13;
+            // 
+            // lblPhoneTwo
+            // 
+            this.lblPhoneTwo.AutoSize = true;
+            this.lblPhoneTwo.Location = new System.Drawing.Point(55, 209);
+            this.lblPhoneTwo.Name = "lblPhoneTwo";
+            this.lblPhoneTwo.Size = new System.Drawing.Size(88, 13);
+            this.lblPhoneTwo.TabIndex = 12;
+            this.lblPhoneTwo.Text = "Numer telefonu 2";
+            // 
+            // cbDegree
+            // 
+            this.cbDegree.FormattingEnabled = true;
+            this.cbDegree.Location = new System.Drawing.Point(58, 119);
+            this.cbDegree.Name = "cbDegree";
+            this.cbDegree.Size = new System.Drawing.Size(164, 21);
+            this.cbDegree.TabIndex = 14;
+            // 
+            // txtRoomNo
+            // 
+            this.txtRoomNo.Location = new System.Drawing.Point(254, 120);
+            this.txtRoomNo.Name = "txtRoomNo";
+            this.txtRoomNo.Size = new System.Drawing.Size(164, 20);
+            this.txtRoomNo.TabIndex = 15;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(238, 279);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 16;
+            this.btnBack.Text = "Powrót";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnSaveInstructor
+            // 
+            this.btnSaveInstructor.Location = new System.Drawing.Point(147, 279);
+            this.btnSaveInstructor.Name = "btnSaveInstructor";
+            this.btnSaveInstructor.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveInstructor.TabIndex = 17;
+            this.btnSaveInstructor.Text = "Zapisz";
+            this.btnSaveInstructor.UseVisualStyleBackColor = true;
+            this.btnSaveInstructor.Click += new System.EventHandler(this.btnSaveInstructor_Click);
             // 
             // InstructorAdd
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(472, 331);
+            this.Controls.Add(this.btnSaveInstructor);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.txtRoomNo);
+            this.Controls.Add(this.cbDegree);
+            this.Controls.Add(this.txtPhoneTwo);
+            this.Controls.Add(this.lblPhoneTwo);
+            this.Controls.Add(this.lblRoomNo);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtPhoneOne);
+            this.Controls.Add(this.lblPhoneOne);
+            this.Controls.Add(this.lblDegree);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblSurname);
+            this.Controls.Add(this.lblName);
             this.Name = "InstructorAdd";
             this.Text = "InstructorAdd";
             this.Load += new System.EventHandler(this.InstructorAdd_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtPhoneOne;
+        private System.Windows.Forms.Label lblPhoneOne;
+        private System.Windows.Forms.Label lblDegree;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblRoomNo;
+        private System.Windows.Forms.TextBox txtPhoneTwo;
+        private System.Windows.Forms.Label lblPhoneTwo;
+        private System.Windows.Forms.ComboBox cbDegree;
+        private System.Windows.Forms.TextBox txtRoomNo;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnSaveInstructor;
     }
 }
--- UniSchedule/ViewForms/AddForms/InstructorAdd.cs
+++ UniSchedule/ViewForms/AddForms/InstructorAdd.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,8 +12,16 @@ using System.Windows.Forms;
 
 namespace UniSchedule.ViewForms.AddForms
 {
+
+    
+
     public partial class InstructorAdd : Form
     {
+
+        private DataSet dsDegreeId;
+        private static string connectionString = ConfigurationManager.ConnectionStrings["UniScheduleDB"].ConnectionString;
+        
+
         public InstructorAdd()
         {
             InitializeComponent();
@@ -19,7 +29,72 @@ namespace UniSchedule.ViewForms.AddForms
 
         private void InstructorAdd_Load(object sender, EventArgs e)
         {
+            AttachDataToDegreeId();
+        }
 
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSaveInstructor_Click(object sender, EventArgs e)
+        {
+            AddInstructorToDB();
+        }
+
+        private void AddInstructorToDB()
+        {
+            string[] instructorDataToSave = GetAllInstructorData().ToArray();
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = connectionString;
+
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "AddNewInstructors";
+            cmd.Parameters.AddWithValue("@Name", instructorDataToSave[0]);
+            cmd.Parameters.AddWithValue("@Surname", instructorDataToSave[1]);
+            cmd.Parameters.AddWithValue("@IdDegree", instructorDataToSave[2]);
+            cmd.Parameters.AddWithValue("@OfficeNo", instructorDataToSave[3]);
+            cmd.Parameters.AddWithValue("@Email", instructorDataToSave[4]);
+            cmd.Parameters.AddWithValue("@Phone1", instructorDataToSave[5]);
+            cmd.Parameters.AddWithValue("@Phone2", instructorDataToSave[6]);
+            conn.Open();
+            MessageBox.Show("Connection opened");
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Command Executed");
+            conn.Close();
+        }
+
+        private List<string> GetAllInstructorData()
+        {
+            List<string> instructorData = new List<string>();
+
+            instructorData.Add(txtName.Text);
+            instructorData.Add(txtSurname.Text);
+            instructorData.Add(cbDegree.Text.ToString());
+            instructorData.Add(txtRoomNo.Text.ToString());
+            instructorData.Add(txtEmail.Text);
+            instructorData.Add(txtPhoneOne.Text);
+            instructorData.Add(txtPhoneTwo.Text);
+
+            return instructorData;
+        }
+
+        private void AttachDataToDegreeId()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = connectionString;
+            conn.Open();
+
+            SqlDataAdapter daSearch = new SqlDataAdapter("SELECT id FROM tbDegree", conn);
+            dsDegreeId = new DataSet();
+            daSearch.Fill(dsDegreeId, "daSearch");
+            cbDegree.ValueMember = "id";
+            cbDegree.DataSource = dsDegreeId.Tables["daSearch"];
+            cbDegree.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbDegree.Enabled = true;
         }
     }
 }
